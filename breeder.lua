910751aefab715d1827b8f3044184df4e4ee279c
DudeItsMarck <marcklaurent@hotmail.com>
2021-01-05 03:54:05 +0100

    monitor please

--- breeder.lua
+++ breeder.lua
@@ -1,8 +1,9 @@
 local breeder = peripheral.wrap("left")
-local monitor = peripheral.wrap("right")
+local monitor = peripheral.find("monitor")
 
 local firstItem = breeder.getItem(1).getMetadata()
 local secondItem = breeder.getItem(2).getMetadata()
 
+monitor.setBackgroundColour(colors.white)
 monitor.write("First slot strength: " .. tostring(firstItem.roost.strength))
 monitor.write("Second slot strength: " .. tostring(secondItem.roost.strength))
