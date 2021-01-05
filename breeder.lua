7a4b5e2e055f472b7cf185fcc1a01685eaf8e6bf
DudeItsMarck <marcklaurent@hotmail.com>
2021-01-05 03:54:47 +0100

    ...

--- breeder.lua
+++ breeder.lua
@@ -5,5 +5,5 @@ local firstItem = breeder.getItem(1).getMetadata()
 local secondItem = breeder.getItem(2).getMetadata()
 
 monitor.setBackgroundColour(colors.white)
-monitor.write("First slot strength: " .. tostring(firstItem.roost.strength))
-monitor.write("Second slot strength: " .. tostring(secondItem.roost.strength))
+-- monitor.write("First slot strength: " .. tostring(firstItem.roost.strength))
+-- monitor.write("Second slot strength: " .. tostring(secondItem.roost.strength))
